3cc4ded13573bf527b1c3e268d28268b4a367df1
ultra <166610445+ultramarine23@users.noreply.github.com>
2025-08-27 17:08:59 +0800

    Added probabilities panel
    still empty as of now tho

--- Links/link.gd
+++ Links/link.gd
@@ -1,14 +1,33 @@
 extends Button
 class_name Link
 
+@onready var popup = $Popup
+
 var link_type : int = Consts.NUMBER
 var link_value # variant variable, can be int, float or callable
+
+var pool_display_mode : bool = false
 
 signal link_pressed(link : Button)
 
 func _ready():
 	pressed.connect(on_pressed)
+	mouse_entered.connect(on_hovered)
+	mouse_exited.connect(on_unhovered)
+	popup.hide()
 
 func on_pressed():
-	link_pressed.emit(self)
-	get_parent().remove_child(self)
+	if not pool_display_mode:
+		link_pressed.emit(self)
+		get_parent().remove_child(self)
+
+func on_hovered():
+	if pool_display_mode:
+		popup.show()
+
+func on_unhovered():
+	popup.hide()
+
+
+func switch_to_pool_display_mode():
+	pool_display_mode = true
